da961835d62e6b128df60aa2480db04e2e45d2c7
hkgunawan <hendrakurniawangunawan@gmail.com>
2020-08-25 16:02:54 +0700

    css problem

--- app/Question.php
+++ app/Question.php
@@ -30,4 +30,14 @@ class Question extends Model
     {
         return $this->created_at->diffForhumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id)
+                return "answered-accepted";
+            return "answered";
+        } else if ($this->answers = 0)
+            return "unanswered";
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,33 +1,43 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">All Questions</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">All Questions</div>
 
-                <div class="card-body">
-                    @foreach($questions as $question)
-                        <div class="media">
-                            <div class="media-body">
-                                <h3 class="mt-0">
-                                    <a href="{{ $question->url }}"> {{ $question->title }}</a>
-                                </h3>
-                                <p class="lead">Asked by
-                                    <a href="{{ $question->user->url }}"> {{ $question->user->name }}</a>
-                                    <small class="text-muted"> {{ $question->created_date }} </small>
-                                </p>
+                    <div class="card-body">
+                        @foreach ($questions as $question)
+                            <div class="media">
+                                <div class="d-flex flex-column counters">
+                                    <div classs="vote">
+                                        <strong>{{ $question->votes }}</strong>{{ Str::plural('vote', $question->votes) }}
+                                    </div>
+                                    <div classs="status answered-accepted">
+                                        <strong>{{ $question->answer }}</strong>{{ Str::plural('answer', $question->answer) }}
+                                    </div>
+                                    <div classs="view">
+                                        {{ $question->views . ' ' . Str::plural('view', $question->views) }}
+                                    </div>
+                                </div>
+                                <div class="media-body">
+                                    <h3 class="mt-0">
+                                        <a href="{{ $question->url }}">{{ $question->title }}</a>
+                                    </h3>
+                                    <p class="lead">Asked by
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                        <small class="text-muted">{{ $question->created_date }}</small>
+                                    </p>
 
-                                {{ Str::limit($question->body,250) }}
+                                    {{ Str::limit($question->body, 250) }}
+                                </div>
                             </div>
-                        </div>
-                        <hr>
-                    @endforeach
-                    {{ $questions->links() }}
+                            <hr>
+                        @endforeach
+                        {{ $questions->links() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
